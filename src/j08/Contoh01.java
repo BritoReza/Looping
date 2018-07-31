5f0c0d3864c4f24c0e82afd721670916eceff831
user <user@DESKTOP-BFBJO5R>
2018-07-31 22:47:01 +0700

    For while do while

---
+++ src/j08/Contoh01.java
@@ -0,0 +1,33 @@
+package j08;
+   
+public class Contoh01 {
+    public static void main(String[] args) {
+        
+        // PERULANGAN FOR
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK MOKLET");
+            
+        }
+        // PERULANGAN WHILE
+        int y = 1;
+        while(1 <= 5)
+        {
+            System.out.println("SMK MOKLET");
+            y++;
+        }
+        
+        // PERULANGAN DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK MOKLET");
+            j++;
+        }
+        while(j <= 5);
+        
+        }
+    }
+    
+
